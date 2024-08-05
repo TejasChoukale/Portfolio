c85f52e312b48b6784bc9a7195f231467a19a70d
TejasChoukale <144011993+TejasChoukale@users.noreply.github.com>
2024-08-05 18:49:31 +0530

    updates
    hello brother

---
+++ PRODIGY_WD_04/index.js
@@ -0,0 +1,29 @@
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const form = event.target;
+    const data = new FormData(form);
+
+    fetch(form.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+            alert("Thank you for your message!");
+            form.reset();
+        } else {
+            response.json().then(data => {
+                if (Object.hasOwn(data, 'errors')) {
+                    alert(data["errors"].map(error => error["message"]).join(", "));
+                } else {
+                    alert("Oops! There was a problem submitting your form");
+                }
+            });
+        }
+    }).catch(error => {
+        alert("Oops! There was a problem submitting your form");
+    });
+});
